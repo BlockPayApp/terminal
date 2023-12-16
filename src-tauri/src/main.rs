ba572c7da8c4bf0824afbf3082f49271a0eb603e
Pengwius <tomekftomekf@gmail.com>
2023-12-17 00:01:15 +0100

    Save passphrase

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,8 +2,28 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use serde_json::json;
 use std::fs::{self, File};
-use std::io::{self, Write};
+use std::io::Write;
 use std::path::Path;
+use bip39::{Language, Mnemonic, MnemonicType, Seed};
+use solana_sdk::{
+    account::from_account,
+    clock::Clock,
+    commitment_config::CommitmentConfig,
+    native_token::lamports_to_sol,
+    signature::{keypair_from_seed, write_keypair_file},
+    signer::Signer,
+    sysvar,
+};
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+    rent::Rent,
+    sysvar::Sysvar,
+};
+
 
 const SETTINGS_PATH: &str = "./../settings.json";
 
@@ -38,9 +58,77 @@ fn save_language(language: &str) -> String {
     "Language saved successfully".to_string()
 }
 
+#[tauri::command]
+fn save_passphrase(passphrase: &str) {
+    let mnemonic = Mnemonic::from_phrase(passphrase, Language::English).unwrap();
+
+    let seed = Seed::new(&mnemonic, "");
+
+    let mut settings = if Path::new(SETTINGS_PATH).exists() {
+        serde_json::from_str(&fs::read_to_string(SETTINGS_PATH).unwrap()).unwrap()
+    } else {
+        json!({})
+    };
+
+    settings["seed"] = json!(seed.as_bytes());
+    let settings = settings.to_string();
+    let mut file = File::create(SETTINGS_PATH).unwrap();
+    file.write_all(settings.as_bytes()).unwrap();
+
+    let keypair = keypair_from_seed(seed.as_bytes()).unwrap();
+    println!("Mnemonic: {:?}", mnemonic);
+    println!("Public key: {}", &keypair.pubkey());
+
+    // Get balance for this account
+    let rpc_client = solana_client::rpc_client::RpcClient::new(
+        "http://localhost:8899".to_string(),
+    );
+    let balance = rpc_client
+        .get_balance(&keypair.pubkey())
+        .expect("get_balance");
+    println!("Balance: {} SOL", lamports_to_sol(balance));
+
+    let to_address = "7nnaPYYRwnNrEHi17LgVxLUJp59eYsdJeL3wLNDdHhnL";
+    let to_pubkey = to_address.parse::<Pubkey>().unwrap();
+
+    let mut transaction = solana_sdk::transaction::Transaction::new_with_payer(
+        &[solana_program::system_instruction::transfer(
+            &keypair.pubkey(),
+            &to_pubkey,
+            1_000_000_000, // 1 SOL in lamports
+        )],
+        Some(&keypair.pubkey()),
+    );
+
+    let (recent_blockhash, _fee_calculator) = rpc_client.get_recent_blockhash().unwrap();
+    transaction.sign(&[&keypair], recent_blockhash);
+
+    // let result = rpc_client
+    //     .send_and_confirm_transaction_with_commitment(
+    //         &transaction,
+    //         CommitmentConfig::confirmed(),
+    //     );
+
+    let result = rpc_client.send_and_confirm_transaction_with_spinner(&transaction);
+
+    match result {
+        Ok(_) => println!("Transaction succeeded"),
+        Err(err) => eprintln!("Transaction failed: {:?}", err),
+    }
+
+
+    let balance = rpc_client
+        .get_balance(&keypair.pubkey())
+        .expect("get_balance");
+    println!("Balance: {} SOL", lamports_to_sol(balance));
+
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![save_language, get_language])
+        .invoke_handler(tauri::generate_handler![save_language, 
+            get_language,
+            save_passphrase,])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/settings/PrivateKey.jsx
+++ src/components/settings/PrivateKey.jsx
@@ -1,23 +1,70 @@
-import React from 'react';
-import { Button } from 'antd';
-import { LeftOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import { Button, Input } from 'antd';
+import { LeftOutlined, UploadOutlined } from '@ant-design/icons';
+import { invoke } from '@tauri-apps/api/tauri';
+
+const { TextArea } = Input;
 
 const PrivateKey = () => {
+  const [passphrase, setPassphrase] = useState('');
+
   const goBack = () => {
     location.hash = '#/settings';
   }
+
+  const handleFileUpload = (event) => {
+    // const file = event.target.files[0];
+    // if (file) {
+    //   const reader = new FileReader();
+    //   reader.onload = (e) => {
+    //     const privateKey = e.target.result;
+    //     // Save the private key using Tauri
+    //     invoke('save_private_key', privateKey);
+    //   };
+    //   reader.readAsText(file);
+    // }
+  };
+
+  const handlePassphraseSubmit = () => {
+    // Save the passphrase using Tauri
+    invoke('save_passphrase', {passphrase: passphrase});
+  };
 
   const containerStyle = {
     margin: 10,
     width: `calc(100vw - ${2 * 10}px)`,
     position: 'fixed',
-    top: 0
+    top: 0,
+    color: 'gray'
+  };
+
+  const backButtonStyle = {
+    position: 'absolute', 
+    left: 0, 
+    top: 0 
   };
 
   return (
     <div style={containerStyle}>
-      <Button icon={<LeftOutlined />} size={'large'} onClick={goBack} style={{ position: 'absolute', left: 0, top: 0 }} />
-      dupa
+      <Button icon={<LeftOutlined />} size={'large'} onClick={goBack} style={backButtonStyle} />
+      <br />
+      <br />
+      <TextArea showCount rows={4} value={passphrase} style={{ resize: 'none' }} onChange={e => setPassphrase(e.target.value)} placeholder="Enter passphrase" />
+      <br />
+      <br />
+      <Button type="primary" size={'large'} style={{ float: 'right' }} onClick={handlePassphraseSubmit}>Submit Passphrase</Button>
+      <br />
+      <br />
+      <h2 style={{ textAlign: 'center' }}>or</h2>
+      <h1 style={{ color: 'black' }}>Import using pendrive</h1>
+      <p style={{ margin: 20 }}>
+        1. Prepare your pendrive{<br />}
+        2. Create a file named <b>private_key.txt</b> in the root directory of your pendrive{<br />}
+        3. Copy your private key into the file{<br />}
+        4. Plug in your pendrive{<br />}
+        5. Click the button below
+      </p>
+      <Button size={'large'} style={{ display: 'flex', justifyContent: 'center', textAlign: 'center', alignItems: 'center', marginLeft: 'auto', marginRight: 'auto' }} icon={<UploadOutlined />}>Import from pendrive</Button>
     </div>
   );
 };
